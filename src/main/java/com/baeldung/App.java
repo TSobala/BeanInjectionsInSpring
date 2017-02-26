ed8adae8bf97a8f6005788cd589509d59fe6fff4
Tomasz <tomasz.sobala@gmail.com>
2017-02-26 17:09:24 +0100

    Bean injections in Spring

---
+++ src/main/java/com/baeldung/App.java
@@ -0,0 +1,17 @@
+package com.baeldung;
+
+import com.baeldung.model.Computer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Application presenting various possibilities of bean injections in Spring.
+ */
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+
+        Computer obj = (Computer) context.getBean("computer");
+        obj.print();
+    }
+}
